e8b9dcbefda0e155c90088ab25a0377d1a62ee73
Douglas Gemir <douglasgemir@gmail.com>
2025-06-04 01:40:53 -0300

    temp

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
-    cpf { '12345678901' } 
+    cpf { '12085172440' } # substitua por um CPF para que o teste funcione
     name { 'João Silva' }
     mail { 'joao@example.com' }
-    password { 'password123' } 
+    password { 'password123' }
     password_confirmation { 'password123' } 
   end
 end
