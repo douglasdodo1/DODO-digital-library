933fab11fd1e3234f752819ff04be7aa2fd8b272
Douglas Gemir <douglasgemir@gmail.com>
2025-06-04 23:39:08 -0300

    feat: book testes

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,20 +7,26 @@ class Book < ApplicationRecord
 
   def validate_isbn
     return errors.add(:isbn, "can't be blank") if isbn.blank?
-    return errors.add(:isbn, "isbn must contain only numbers") unless isbn.match?(/\A\d+\z/)
 
-    number_isbn = isbn.chars.map(&:to_i)
-    errors.add(:isbn, "isbn must contain 13 digits") unless number_isbn.size == 13
+    clean_isbn = isbn.gsub(/\D/, '')
 
-    sum = 0
-    number_isbn[0..11].each_with_index do |digit, index|
-      sum += digit * (index.even? ? 1 : 3)
+    unless clean_isbn.match?(/\A\d{13}\z/)
+      return errors.add(:isbn, "must contain exactly 13 digits")
     end
 
-    check_digit = (10 - (sum % 10)) %10
+    digits = clean_isbn.chars.map(&:to_i)
 
-    unless check_digit == number_isbn[12]
+
+
+    sum = digits[0..11].each_with_index.sum do |digit, index|
+      index.even? ? digit : digit * 3
+    end
+
+    check_digit = (10 - (sum % 10)) % 10
+
+    unless check_digit == digits[12]
       errors.add(:isbn, "is not valid")
     end
   end
+
 end
