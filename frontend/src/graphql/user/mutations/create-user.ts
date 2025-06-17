f3e225938fbf4d0dd55a0fe0a708d8ab5727f902
douglas <carvalhodouglas95@gmail.com>
2025-06-17 00:24:00 -0300

    feat: handle para criar usuario

---
+++ frontend/src/graphql/user/mutations/create-user.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { CREATE_USER } from "../query/create-user-query";
+import { CreateUserInput } from "@/app/inputs/create-user-input";
+
+export async function createUser(input: CreateUserInput) {
+  try {
+    const response = await axios.post(
+      "http://localhost:3000/graphql",
+      {
+        query: CREATE_USER,
+        variables: { input },
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage["token"]}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    console.log(`resposta: ${response.data}`);
+    return response.data;
+  } catch (error) {
+    console.error("Erro ao criar usuario:", error);
+    throw error;
+  }
+}
